d99b1e37544631bc9c00dcb88ce6fed62de496b4
StepXo <66577156+StepXo@users.noreply.github.com>
2022-10-07 18:43:22 -0500

    instanciar letras

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -7,6 +7,7 @@ public class Alfabeto extends Pictograma{
 
     public Alfabeto(String origen,String[] letras, String interpretacion) {
         super(origen);
+        Alfabeto.letras =letras;
         this.interpretacion = interpretacion;
     }
 
