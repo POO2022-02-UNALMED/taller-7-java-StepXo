71e369dd6782f75d2a19fe05ae1a0e9cd317cc19
StepXo <66577156+StepXo@users.noreply.github.com>
2022-10-07 18:34:15 -0500

    primer intento

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,38 @@
+package comunicacion;
+
+
+public class Alfabeto extends Pictograma{
+    private static String[] letras;
+    private String interpretacion;
+
+    public Alfabeto(String origen,String[] letras, String interpretacion) {
+        super(origen);
+        this.interpretacion = interpretacion;
+    }
+
+    public int cantidadLetras(){
+
+        return letras.length;
+    }
+
+    @Override
+    String interpretacion() {
+        return getInterpretacion();
+    }
+
+    @Override
+    public String toString() {
+        String alpahbeto= null;
+        for (int i=0;i < letras.length-1;i++){
+            alpahbeto += letras[i] + ", ";
+        }
+        return alpahbeto;
+    }
+
+    //getters y setters
+    public static String[] getLetras() {return letras;}
+    public static void setLetras(String[] letras) {Alfabeto.letras = letras;}
+
+    public String getInterpretacion() {return interpretacion;}
+    public void setInterpretacion(String interpretacion) {this.interpretacion = interpretacion;}
+}
---
+++ src/main/java/comunicacion/Periodico.java
@@ -0,0 +1,45 @@
+package comunicacion;
+
+public class Periodico extends  Escrito{
+    private String fecha;
+
+    String primicia;
+    private String interpretacion;
+
+    public Periodico(String origen, String titulo, String autor, int paginas, String fecha, String primicia, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.fecha = fecha;
+        this.primicia = primicia;
+
+        this. interpretacion = interpretacion;
+    }
+
+    @Override
+    int palabrasTotales(int palabrasPagina) {
+        return this.getPaginas()*10*palabrasPagina;
+    }
+
+    @Override
+    String interpretacion() {
+        return getInterpretacion();
+    }
+
+    @Override
+    public String toString() {
+        return  super.getOrigen()+"\n"+
+                super.getTitulo()+"\n"+
+                super.getAutor()+"\n"+
+                super.getPaginas()+"\n"+
+                getFecha()+"\n"+
+                getPrimicia();
+    }
+
+    public String getFecha() {return fecha;}
+    public void setFecha(String fecha) {this.fecha = fecha;}
+
+    public String getPrimicia() {return primicia;}
+    public void setPrimicia(String primicia) {this.primicia = primicia;}
+
+    public String getInterpretacion() {return interpretacion;}
+    public void setInterpretacion(String interpretacion) {this.interpretacion = interpretacion;}
+}
